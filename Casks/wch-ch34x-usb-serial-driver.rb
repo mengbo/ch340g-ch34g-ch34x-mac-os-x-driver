d9763595d58fa5f401fe5ae24da8999332fc184c
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2018-04-15 09:50:06 +0200

    Fix deprecated calls

--- Casks/wch-ch34x-usb-serial-driver.rb
+++ Casks/wch-ch34x-usb-serial-driver.rb
@@ -6,8 +6,6 @@ cask 'wch-ch34x-usb-serial-driver' do
   name 'WCH USB serial driver for CH340/CH341 (unofficial release)'
   homepage 'https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver'
 
-  license :gratis
-
   pkg 'CH34x_Install_V1.4.pkg'
 
   uninstall pkgutil: 'com.wch.usbserial',
@@ -17,7 +15,7 @@ cask 'wch-ch34x-usb-serial-driver' do
                        '/Library/Extensions/usbserial.kext',
                      ]
 
-  caveats <<-EOS.undent
+  caveats <<~EOS
     This driver was sourced from the OEM website. Discussion:
     https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver
   EOS
